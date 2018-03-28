030246ad43c1fe040cca273939bef30db2919126
Gustavo Banegas <gustavo@cryptme.in>
2018-03-28 22:37:41 +0200

    created class for Rotation Gate

---
+++ src/main/java/libQ/gates/RotZGate.java
@@ -0,0 +1,45 @@
+/**
+ * 
+ */
+package libQ.gates;
+
+import java.rmi.UnexpectedException;
+
+import libQ.exceptions.OperationNotPermittedException;
+import libQ.register.QReg;
+
+/**
+ * @author Gustavo Banegas
+ *
+ */
+public class RotZGate implements IGate {
+
+	/* (non-Javadoc)
+	 * @see libQ.gates.IGate#apply(libQ.register.QReg, int)
+	 */
+	@Override
+	public Boolean apply(QReg reg, int targetQBit) throws OperationNotPermittedException, UnexpectedException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/* (non-Javadoc)
+	 * @see libQ.gates.IGate#apply(libQ.register.QReg, int, int)
+	 */
+	@Override
+	public Boolean apply(QReg reg, int controlQBit, int targetQBit) throws OperationNotPermittedException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/* (non-Javadoc)
+	 * @see libQ.gates.IGate#apply(libQ.register.QReg, int, int, int)
+	 */
+	@Override
+	public Boolean apply(QReg reg, int controlQBit1, int controlQBit2, int targetQBit)
+			throws OperationNotPermittedException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
