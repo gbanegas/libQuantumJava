002056f8f4f479852e91490a1d03e8dc06099cb9
Gustavo Banegas <gustavo@cryptme.in>
2018-07-05 15:46:10 +0200

    removed unused import

--- src/main/java/libQ/circuit/draw/CircuitDraw.java
+++ src/main/java/libQ/circuit/draw/CircuitDraw.java
@@ -13,7 +13,6 @@ import java.util.HashMap;
 import java.util.List;
 
 import javax.swing.JPanel;
-import javax.swing.JScrollPane;
 
 import libQ.circuit.QuantumOperation;
 import libQ.circuit.TimeStampQuantum;
